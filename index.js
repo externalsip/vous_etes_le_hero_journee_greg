a835f344e2285044a88cd27500e22f165106a9f9
externalsip <93773873+externalsip@users.noreply.github.com>
2022-09-22 17:00:57 -0400

    commit 1, 22/09/22
    added script, and some chapters

---
+++ index.js
@@ -0,0 +1,37 @@
+let chaptersObj = {
+
+    introduction: {    
+        subtitle: "Introduction",
+        text: "Greg est un être humain normal, il vit seul et travail dans un bureau régulier dans lequel il risque de passer le restant de ses jours, L'histoire dont vous allez témoigner de est l'une des journées banales qui composent sa vie.",
+        img: "assets/thumbs_up_man2.jpg"},
+    matin: {
+        subtitle: "Matin",
+        text: "Greg se réveille, il est prêt à affronter sa journée mais n'est pas certain de ce qu'il devrait faire en cet instant. Que devrait-il faire?"
+
+    },
+    nouveau_matin: {
+        subtitle: "Prise 2",
+        text: "Après s'être rendormi pour une durée de temps indéterminée, Greg se réveil, il est encore une fois prêt à affronter sa journée. Que devrait-il faire?"
+    
+    },
+    wow: {
+        subtitle: "wow.",
+        text: "Greg se rendort, mais ne réveillera pas, apparement les êtres vivants ont besoin de se nourrir pour rester en vie, quelque chose que Greg n'a pas vraiment eu la chance de faire dans les 48 dernières heures comme celui-ci était endormi."
+
+    },
+    retard: {
+        subtitle: "En retard!!!",
+        text: "Greg décide de se faire à déjeuner, il ouvre son frigo et trouve de quoi manger, il décide de faire des oeufs, Greg est connu pour ses très bons oeufs"
+
+
+    }
+    
+
+};
+
+function goToChapter(chapterName){
+
+    console.log(chaptersObj.introduction.subtitle);
+    console.log(chaptersObj.introduction.text);
+
+}
